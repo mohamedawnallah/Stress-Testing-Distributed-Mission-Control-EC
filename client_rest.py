d19b048331a95a6d9d3b1f0929efe5dcd32c7d25
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2024-06-25 01:04:54 +0300

    client_rest.py: make fail and success amt non-zero
    In this commit, we make fail and success amounts
    non-zero to avoid multiple cases where this returns
    an RPC error from the EC.

--- client_rest.py
+++ client_rest.py
@@ -88,8 +88,8 @@ def generate_random_history():
     fail_time = random.randint(current_time - one_week, current_time)
     success_time = random.randint(current_time - one_week, current_time)
 
-    fail_amt_sat = random.randint(0, 10000)
-    success_amt_sat = random.randint(0, 10000)
+    fail_amt_sat = random.randint(1, 10000)
+    success_amt_sat = random.randint(1, 10000)
 
     return {
         "fail_time": fail_time,
